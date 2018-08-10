044d6aa45d62cae3b9cadf5d9e2eb444e794a0e1
reiji <31179220+reud@users.noreply.github.com>
2018-08-11 00:20:50 +0900

    popopo

--- filenameToText.py
+++ filenameToText.py
@@ -1,9 +1,11 @@
 import os
-#At imagesFolder,get filename and output to filenames.txt
+import sys
+arg=sys.argv[1]
+#At imagesFolder,get filename and output to arg1
 if __name__=="__main__":
     path="./images"
     files=os.listdir(path)
-    f=open("filenames.txt",'w')
+    f=open(arg,'w')
     for string in files:
         f.write(string+'\n')
     f.close()
--- indexFaces.py
+++ indexFaces.py
@@ -1,5 +1,21 @@
 import boto3
 import  sys
 import  json
-#commandline argument: (arg1,arg2)
-#dont use it if you had corretion
+#commandline argument: (arg1,arg2,arg3)=(filename,bucketname,collectionID)
+if __name__=="__main__":
+    args = sys.argv
+    bucket=args[2]
+    collectionID=args[3]
+    file=open(args[1])
+    data1=file.read()
+    file.close()
+    lines=data1.split("\n")
+    client = boto3.client('rekognition','us-east-1')
+    for lineFileName in lines:
+        response=client.index_faces(CollectionId=collectionID,
+                                    Image={'S3Object':{'Bucket':bucket,'Name':lineFileName}},
+                                    ExternalImageId=lineFileName,
+                                    DetectionAttributes=['ALL'])
+        print(f'Faces in {lineFileName}')
+        for faceRecord in response['FaceRecord']:
+            print(faceRecord['Face']['FaceId'])
